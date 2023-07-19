a9a574c28117b7d6c4a5fb5047169dc0f2ddf070
DeepaG2001 <deepag.xworkz@gmail.com>
2023-07-19 23:39:25 +0530

    message

---
+++ Deepa/Place.java
@@ -0,0 +1,10 @@
+class Place{
+	public static void main(String[] args){
+		System.out.println("I am from Harihara");
+		System.out.println("Harihara is a city in davanagere District ");
+		System.out.println("Harihar is famous for Harihareswara temple");
+		System.out.println("Harihara is situated on the banks of the Tungabhadra River");
+		System.out.println("Harihara and Davanagere are referred as 'Twin City'");
+		
+	}
+}
